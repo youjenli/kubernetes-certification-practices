64acb023a1f61f81ebece71c423333d4945619be
youjenli <youjenli1124@gmail.com>
2024-01-08 00:30:37 +0800

    Remove VPC resource because I am not able to delete it after the removal of k8s cluster.

--- modules/kubernetes/network.tf
+++
@@ -1,6 +0,0 @@
-resource "digitalocean_vpc" "k8s_vpc" {
-  name     = "kubernetes-demo-project-network"
-  region   = var.region
-  ip_range = "10.106.0.0/20"
-}
-# Note: vpc could not be a project resource.
