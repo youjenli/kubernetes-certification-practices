2fbc3102f7dc26ae680196fdc266132c74ea0374
youjenli <youjenli1124@gmail.com>
2024-01-08 00:17:35 +0800

    Remove project from module because it may not be deleted after k8s is removed.

--- modules/kubernetes/main.tf
+++ modules/kubernetes/main.tf
@@ -1,12 +1,5 @@
-resource "digitalocean_project" "playground" {
-  name        = "playground"
-  description = "A project that organizes resources established for DigitalOcean feature exploration."
-  purpose     = "Exploration"
-  environment = "Development"
-}
-
 resource "digitalocean_kubernetes_cluster" "k8s_cluster" {
-  name   = "kubernetes-cluster-demo"
+  name   = "kubernetes-cluster-playground"
   region = var.region
   # Grab the latest version slug from `doctl kubernetes options versions`
   version = "1.28.2-do.0"
@@ -28,12 +21,5 @@ resource "digitalocean_kubernetes_cluster" "k8s_cluster" {
 
   depends_on = [
     digitalocean_container_registry.registry
-  ]
-}
-
-resource "digitalocean_project_resources" "module_resources" {
-  project = digitalocean_project.playground.id
-  resources = [
-    digitalocean_kubernetes_cluster.k8s_cluster.urn
   ]
 }
--- modules/kubernetes/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "project_id" {
-  value = digitalocean_project.playground.id
-}
