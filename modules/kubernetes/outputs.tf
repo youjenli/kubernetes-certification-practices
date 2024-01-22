196f34b9fdf5cfc168e41b26de241e279137f691
youjenli <youjenli1124@gmail.com>
2024-01-23 00:25:49 +0800

    Print docker registry credentials

--- modules/kubernetes/outputs.tf
+++ modules/kubernetes/outputs.tf
@@ -1,3 +1,7 @@
 output "k8s_version" {
   value = "${digitalocean_kubernetes_cluster.k8s_cluster.version}"
 }
+
+output "docker_credentials_expiration_time" {
+  value = digitalocean_container_registry_docker_credentials.registry.credential_expiration_time
+}
