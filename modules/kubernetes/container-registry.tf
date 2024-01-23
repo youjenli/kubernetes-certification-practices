517106173213bb2bda7bf1834851d89b0e2ff8e5
youjenli <youjenli1124@gmail.com>
2024-01-24 00:07:05 +0800

    Login DigitalOcean registry with decrypted user name and password.

--- modules/kubernetes/container-registry.tf
+++ modules/kubernetes/container-registry.tf
@@ -3,16 +3,3 @@ resource "digitalocean_container_registry" "registry" {
   subscription_tier_slug = "basic"
   region                 = var.region
 }
-
-resource "digitalocean_container_registry_docker_credentials" "registry" {
-  registry_name = var.container_registry_name
-  depends_on = [
-    digitalocean_container_registry.registry
-  ]
-}
-
-resource "local_file" "docker_credentials" {
-  depends_on = [digitalocean_container_registry_docker_credentials.registry]
-  content    = digitalocean_container_registry_docker_credentials.registry.docker_credentials
-  filename   = "${var.config_path}/docker.json"
-}
--- run.ps1
+++ run.ps1
@@ -3,16 +3,29 @@ param(
     [bool] $aa = $False
 )
 
+function Docker-Login ([string]$do_token)
+{
+    docker login -u $do_token -p $do_token registry.digitalocean.com
+}
+
+function Docker-Logout
+{
+    docker logout registry.digitalocean.com
+}
+
+$docker_handler = $null
 $do_token = Get-Content .\access-token.txt -Raw
 Push-Location -Path .\digitalocean\SFO3\kubernetes\
 
 IF ($cmd -eq "ap")
 {
     $cmd = "apply"
+    $docker_handler = "Docker-Login"
 }
 IF ($cmd -eq "dt")
 {
     $cmd = "destroy"
+    $docker_handler = "Docker-Logout"
 }
 
 $auto_approve = ""
@@ -25,3 +38,5 @@ terragrunt init
 terragrunt $cmd -lock=false -var do_token=$do_token $auto_approve
 
 Pop-Location
+
+& $docker_handler $do_token
