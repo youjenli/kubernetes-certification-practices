ec5db03babdc0bf5aaf3ea0f77dbe2735f362df5
Martin Lee <youjenli1124@gmail.com>
2024-05-18 21:57:31 +0800

    Add check points to all provision scripts.

--- apply.ps1
+++ apply.ps1
@@ -15,15 +15,33 @@ if ( $aa -eq $True )
 Push-Location -Path $PSScriptRoot\terragrunt\digitalocean\SFO3\kubernetes-certification\container-registry
 terragrunt init
 terragrunt apply -lock=false -var do_token=$Env:do_token $auto_approve $non_interactive -var-file $tf_vars_path
-docker login -u $Env:do_token -p $Env:do_token registry.digitalocean.com
+$exit_code = $?
+if ( $exit_code -eq $True)
+{
+    docker login -u $Env:do_token -p $Env:do_token registry.digitalocean.com
+}
 Pop-Location
+if ( $exit_code -ne $True )
+{
+    exit $exit_code
+}
 
 Push-Location -Path $PSScriptRoot\terragrunt\digitalocean\SFO3\kubernetes-certification\kubernetes-cluster
 terragrunt init
 terragrunt apply -lock=false -var do_token=$Env:do_token $auto_approve $non_interactive -var-file $tf_vars_path
+$exit_code = $?
 Pop-Location
+if ( $exit_code -ne $True )
+{
+    exit $exit_code
+}
 
 Push-Location -Path $PSScriptRoot\terragrunt\digitalocean\SFO3\kubernetes-certification\kubernetes
 terragrunt init
 terragrunt apply -lock=false -var do_token=$Env:do_token $auto_approve $non_interactive
+$exit_code = $?
 Pop-Location
+if ( $exit_code -ne $True )
+{
+    exit $exit_code
+}
--- apply.sh
+++ apply.sh
@@ -21,15 +21,29 @@ source $APPLY_SCRIPT_PATH/init.sh
 cd $INIT_SCRIPT_PATH/terragrunt/digitalocean/SFO3/kubernetes-certification/container-registry
 terragrunt init
 terragrunt apply -lock=false -var do_token=$DO_TOKEN $AUTO_APPROVE $NONE_INTERACTIVE -var-file $TF_VARS_PATH
-export REGISTRY_ENDPOINT=$(terragrunt output -raw registry_endpoint)
+EXIT_CODE=$?
+if [ $EXIT_CODE -eq 0 ]; then
+  export REGISTRY_ENDPOINT=$(terragrunt output -raw registry_endpoint)
+fi
 cd -
+if [ $EXIT_CODE -ne 0 ]; then
+  exit $EXIT_CODE
+fi
 
 cd $INIT_SCRIPT_PATH/terragrunt/digitalocean/SFO3/kubernetes-certification/kubernetes-cluster
 terragrunt init
 terragrunt apply -lock=false -var do_token=$DO_TOKEN $AUTO_APPROVE $NONE_INTERACTIVE -var-file $TF_VARS_PATH
+EXIT_CODE=$?
 cd -
+if [ $EXIT_CODE -ne 0 ]; then
+  exit $EXIT_CODE
+fi
 
 cd $INIT_SCRIPT_PATH/terragrunt/digitalocean/SFO3/kubernetes-certification/kubernetes
 terragrunt init
 terragrunt apply -lock=false -var do_token=$DO_TOKEN $AUTO_APPROVE $NONE_INTERACTIVE
+EXIT_CODE=$?
 cd -
+if [ $EXIT_CODE -ne 0 ]; then
+  exit $EXIT_CODE
+fi
