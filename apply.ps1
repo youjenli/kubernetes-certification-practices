a23652c01be85c4a78ddf5a4545084c9bdfb10fd
youjenli <youjenli1124@gmail.com>
2024-06-08 17:55:36 +0800

    Fix the system initialization powershell script.

--- apply.ps1
+++ apply.ps1
@@ -14,7 +14,7 @@ if ( $aa -eq $True )
 
 Push-Location -Path $PSScriptRoot\terragrunt\digitalocean\SFO3\kubernetes-certification\container-registry
 terragrunt init
-terragrunt apply -lock=false -var do_token=$Env:do_token $auto_approve $non_interactive -var-file $tf_vars_path
+terragrunt apply -lock=false -var do_token=$Env:do_token $auto_approve $non_interactive -var-file $Env:tf_vars_path
 $exit_code = $?
 if ( $exit_code -eq $True)
 {
@@ -28,7 +28,7 @@ if ( $exit_code -ne $True )
 
 Push-Location -Path $PSScriptRoot\terragrunt\digitalocean\SFO3\kubernetes-certification\kubernetes-cluster
 terragrunt init
-terragrunt apply -lock=false -var do_token=$Env:do_token $auto_approve $non_interactive -var-file $tf_vars_path
+terragrunt apply -lock=false -var do_token=$Env:do_token $auto_approve $non_interactive -var-file $Env:tf_vars_path
 $exit_code = $?
 Pop-Location
 if ( $exit_code -ne $True )
